fb7860cd44667b9603ad4bf993ee60814d007d7e
Mikhail <manosov14@gmail.com>
2023-10-10 03:06:15 +0300

    init commit

---
+++ src/main/kotlin/com/minibank/accounts/MiniBankAccountsApplication.kt
@@ -0,0 +1,11 @@
+package com.minibank.accounts
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MiniBankAccountsApplication
+
+fun main(args: Array<String>) {
+	runApplication<MiniBankAccountsApplication>(*args)
+}
---
+++ src/test/kotlin/com/minibank/accounts/MiniBankAccountsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.minibank.accounts
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MiniBankAccountsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
